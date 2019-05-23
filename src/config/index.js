80eefe57f9df61be1f0055036b0d5de73a1b7027
XHFkindergarten <791319924@qq.com>
2019-05-24 01:16:14 +0800

    5-23凌晨第一次生产测试

--- src/config/index.js
+++ src/config/index.js
@@ -1,6 +1,7 @@
-const devHost = 'http://localhost:4000'
-const prdHost = 'http://www.xhfkindergarten.cn:4000'
-const host = process.env.NODE_ENV === 'development' ? devHost : prdHost
+// const devHost = 'http://localhost:4000'
+const prdHost = 'https://www.xhfkindergarten.cn:4000'
+// const host = process.env.NODE_ENV === 'development' ? devHost : prdHost
+const host = prdHost
 const imgHost = 'https://img.xhfkindergarten.cn'
 export default {
   host,
